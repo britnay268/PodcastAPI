c7178d782a7f359521071c8a4ff216969ffc1b1a
Britnay Gore <britnaygore268@gmail.com>
2024-10-15 20:27:02 -0400

    Create interface for podcasts

--- Interfaces/IPodcastRepository.cs
+++ Interfaces/IPodcastRepository.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using PodcastAPI.Models;
+
 namespace PodcastAPI.Interfaces
 {
-	public class IPodcastRepository
+	public interface IPodcastRepository
 	{
-		public IPodcastRepository()
-		{
-		}
-	}
+        Task<List<Podcast>> GetPodcastsAsync(int userId);
+
+        Task<Podcast> GetPodcastByIdAsync(int userId);
+
+        Task<List<Podcast>> GetFavoritePodcastsAsync(int userId);
+
+        Task<Podcast> CreatePodcastAsync(Podcast podcast);
+
+        Task<Podcast> UpdatePodcastAsync(int id, Podcast podcast);
+
+        Task<Podcast> DeletePodcastAsync(int id);
+
+        Task<FavoritePodcast> ToggleFavoritePodcastAsync(int podcastId, int userId);
+    }
 }
 
