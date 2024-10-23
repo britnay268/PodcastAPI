962b67e01bdf2a187af806e9e67385a775ed960f
Alex Berka <alexanderberka@gmail.com>
2024-10-22 20:58:29 -0500

    Added getSingleEpisode, EpisodeSubmitDTO for create and update

--- Interfaces/IEpisodeService.cs
+++ Interfaces/IEpisodeService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PodcastAPI.DTOs;
 using PodcastAPI.Models;
 
 namespace PodcastAPI.Interfaces;
@@ -7,9 +8,11 @@ public interface IEpisodeService
 {
     Task<List<User>> GetFavoriteEpisodesAsync(int userId);
 
-    Task<Episode> CreateEpisodeAsync(Episode episode);
+    Task<Episode> GetSingleEpisode(int episodeId);
 
-    Task<Episode> UpdateEpisodeAsync(int id, Episode episode);
+    Task<Episode> CreateEpisodeAsync(EpisodeSubmitDTO episodeSubmit);
+
+    Task<Episode> UpdateEpisodeAsync(int id, EpisodeSubmitDTO episodeSubmit);
 
     Task<Episode> DeleteEpisodeAsync(int id);
 
