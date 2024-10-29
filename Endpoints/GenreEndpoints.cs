eb8a4079749ff82b89ac4f5586d265744a643edd
Britnay Gore <britnaygore268@gmail.com>
2024-10-29 19:17:08 -0400

    Added endpoints to get all genres and get podcasts by genre

--- Endpoints/GenreEndpoints.cs
+++ Endpoints/GenreEndpoints.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using PodcastAPI.Models;
+using PodcastAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace PodcastAPI.Endpoints
 {
@@ -8,6 +10,52 @@ namespace PodcastAPI.Endpoints
         public static void MapGenreEndpoints(this IEndpointRouteBuilder routes)
         {
             var group = routes.MapGroup("").WithTags(nameof(Genre));
+
+            group.MapGet("/genres", (PodcastAPIDbContext db) =>
+            {
+                return db.Genres.Select(genre => new
+                {
+                    Id = genre.Id,
+                    Name = genre.Name,
+                });
+            });
+
+            group.MapGet("/podcast/genre/{genreId}", async (PodcastAPIDbContext db, int genreId, int userFavoritesId) =>
+            {
+                var podcastByGenre = await db.Podcasts
+                    .Where(p => p.GenreId == genreId)
+                    .Include(p => p.User)
+                    .Include(p => p.Genre)
+                    .Include(p => p.Episodes)
+                    .Include(p => p.UsersFavorited)
+                    .OrderBy(p => p.Title)
+                    .ToListAsync();
+
+                if (podcastByGenre.Count == 0)
+                {
+                    return Results.NotFound("There are no podcast with the genreId.");
+                }
+
+                return Results.Ok(podcastByGenre.Select(pg => new
+                {
+                    pg.Id,
+                    pg.Title,
+                    pg.Description,
+                    pg.ImageUrl,
+                    pg.CreatedOn,
+                    pg.Genre,
+                    User = new
+                    {
+                        pg.User.Id,
+                        pg.User.Username,
+                        pg.User.FirstName,
+                        pg.User.LastName,
+                        pg.User.ImageUrl
+                    },
+                    EpisodeCount = pg.Episodes.Count,
+                    Favorite = pg.UsersFavorited.Any(uf => uf.Id == userFavoritesId)
+                }));
+            });
         }
     }
 }
