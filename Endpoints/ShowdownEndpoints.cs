bcc6deee580aaf6e8e6ec6346a9c26783c0a1c2e
Britnay Gore <britnaygore268@gmail.com>
2024-11-02 10:37:49 -0400

    Created endpoint file for showdown and added line in program.cs to show apis in swagger for showdown

---
+++ Endpoints/ShowdownEndpoints.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PodcastAPI.Endpoints;
+
+public static class ShowdownEndpoints
+{
+	public static void MapShowdownEndpoints(this IEndpointRouteBuilder routes)
+	{
+	}
+}
+
